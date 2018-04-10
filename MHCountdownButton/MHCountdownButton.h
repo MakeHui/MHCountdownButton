d36c25d2f017bc58eff7cd01e717f862922d7031
MakeHui <makehuir@gmail.com>
2018-04-10 23:23:55 +0800

    add carthage support

--- MHCountdownButton/MHCountdownButton.h
+++ MHCountdownButton/MHCountdownButton.h
@@ -1,12 +1,20 @@
 //
 //  MHCountdownButton.h
-//  MHCountdownButtonExample
+//  MHCountdownButton
 //
 //  Created by MakeHui on 2018/4/10.
 //  Copyright © 2018年 MakeHui. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+
+//! Project version number for MHCountdownButton.
+FOUNDATION_EXPORT double MHCountdownButtonVersionNumber;
+
+//! Project version string for MHCountdownButton.
+FOUNDATION_EXPORT const unsigned char MHCountdownButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHCountdownButton/PublicHeader.h>
 
 @protocol MHCountdownButtonDelegate;
 
--- MHCountdownButton/MHCountdownButton.m
+++ MHCountdownButton/MHCountdownButton.m
@@ -1,6 +1,6 @@
 //
 //  MHCountdownButton.m
-//  MHCountdownButtonExample
+//  MHCountdownButton
 //
 //  Created by MakeHui on 2018/4/10.
 //  Copyright © 2018年 MakeHui. All rights reserved.
